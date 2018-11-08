26f8b2fda5373f359ad84e058d9f94f33f79496d
thatzenbuhler <t.hatzenbuhler@gmail.com>
2018-11-07 17:00:36 -0800

    Initial

---
+++ app/src/main/java/edu/fullerton/csu/hatzenbuhler/tyler/application1/CalculatorActivity.kt
@@ -0,0 +1,28 @@
+package edu.fullerton.csu.hatzenbuhler.tyler.application1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import kotlinx.android.synthetic.main.activity_calculator.*
+
+class CalculatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        if(message == "0"){
+            calcTitle.text = "Calculator 1"
+        }
+        else if(message == "1"){
+            calcTitle.text = "Calculator 2"
+        }
+        else if(message == "2"){
+            calcTitle.text = "Calculator 3"
+        }
+        else if(message == "3"){
+            calcTitle.text = "Calculator 4"
+        }
+    }
+}
---
+++ app/src/main/java/edu/fullerton/csu/hatzenbuhler/tyler/application1/MainActivity.kt
@@ -0,0 +1,34 @@
+package edu.fullerton.csu.hatzenbuhler.tyler.application1
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+val calcList = arrayOf(
+    "Calculator 1",
+    "Calculator 2",
+    "Calculator 3",
+    "Calculator 4"
+)
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, calcList)
+
+        listView.adapter = adapter
+        listView.setOnItemClickListener{_,_, position, _ ->
+            val message = position.toString()
+            val intent = Intent(this, CalculatorActivity::class.java).apply{
+                putExtra(EXTRA_MESSAGE, message)
+            }
+            startActivity(intent)
+        }
+    }
+}
